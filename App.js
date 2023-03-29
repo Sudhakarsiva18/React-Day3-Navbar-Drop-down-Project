ab8c0f120de733129c92ba61f7ae2911dfd1bff4
Sudhakarsiva18 <108916787+Sudhakarsiva18@users.noreply.github.com>
2023-03-29 14:01:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Menu from './day3/Class/Menu'
+
+function App() {
+  return (
+    <div>
+      <Menu/>
+    </div>
+  )
+}
+
+export default App
